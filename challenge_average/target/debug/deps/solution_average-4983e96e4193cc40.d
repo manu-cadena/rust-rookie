5d5d44c879a382026838b375c10f5d32b45e79d1
Manuel <69732401+manu-cadena@users.noreply.github.com>
2024-10-26 15:52:04 +0200

    Add challenge_average program

---
+++ challenge_average/target/debug/deps/solution_average-4983e96e4193cc40.d
@@ -0,0 +1,5 @@
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_average/target/debug/deps/solution_average-4983e96e4193cc40: src/main.rs
+
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_average/target/debug/deps/solution_average-4983e96e4193cc40.d: src/main.rs
+
+src/main.rs:
---
+++ challenge_average/target/debug/solution_average.d
@@ -0,0 +1,1 @@
+/Users/manulopez/Blockchain\ Utveckling/Rust/rust-rookie/challenge_average/target/debug/solution_average: /Users/manulopez/Blockchain\ Utveckling/Rust/rust-rookie/challenge_average/src/main.rs
