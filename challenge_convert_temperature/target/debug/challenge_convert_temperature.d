7aa43b12814f287bc978be8cd0e5ee93d13b9940
Manuel <69732401+manu-cadena@users.noreply.github.com>
2024-10-28 20:37:14 +0100

    Added and passed convert temperature challenge

---
+++ challenge_convert_temperature/target/debug/challenge_convert_temperature.d
@@ -0,0 +1,1 @@
+/Users/manulopez/Desktop/challenge_convert_temperature/target/debug/challenge_convert_temperature: /Users/manulopez/Desktop/challenge_convert_temperature/src/main.rs
---
+++ challenge_convert_temperature/target/debug/deps/challenge_convert_temperature-edbaef8b9be03555.d
@@ -0,0 +1,5 @@
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_convert_temperature/target/debug/deps/challenge_convert_temperature-edbaef8b9be03555: src/main.rs
+
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_convert_temperature/target/debug/deps/challenge_convert_temperature-edbaef8b9be03555.d: src/main.rs
+
+src/main.rs:
