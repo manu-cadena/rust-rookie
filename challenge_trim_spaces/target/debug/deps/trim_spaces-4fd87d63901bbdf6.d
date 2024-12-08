a0476f8c58cf3d61818f92a9246162e49eb30463
Manuel <69732401+manu-cadena@users.noreply.github.com>
2024-12-08 14:31:21 +0100

    Passed challenge trim spaces

---
+++ challenge_trim_spaces/target/debug/deps/trim_spaces-4fd87d63901bbdf6.d
@@ -0,0 +1,5 @@
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_trim_spaces/target/debug/deps/trim_spaces-4fd87d63901bbdf6: src/main.rs
+
+/Users/manulopez/Blockchain Utveckling/Rust/rust-rookie/challenge_trim_spaces/target/debug/deps/trim_spaces-4fd87d63901bbdf6.d: src/main.rs
+
+src/main.rs:
---
+++ challenge_trim_spaces/target/debug/trim_spaces.d
@@ -0,0 +1,1 @@
+/Users/manulopez/Blockchain\ Utveckling/Rust/rust-rookie/challenge_trim_spaces/target/debug/trim_spaces: /Users/manulopez/Blockchain\ Utveckling/Rust/rust-rookie/challenge_trim_spaces/src/main.rs
